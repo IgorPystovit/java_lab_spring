27cde3024b438cb99e58b2f032e4574431a49bdf
Igor Pystovit <igor.pystovit@gmail.com>
2019-05-11 14:57:11 +0300

    multithreading/EndlessPingPong.java added

---
+++ multithreading/pingpong/EndlessPingPong.java
@@ -0,0 +1,53 @@
+package multithreading.pingpong;
+
+public class EndlessPingPong {
+    private static Object screenlocker = new Object();
+    private Thread ping = new Thread(()->{
+        synchronized (screenlocker){
+            while (true){
+                try{
+                    screenlocker.wait();
+                    System.out.println("Ping");
+                    Thread.sleep(1000);
+                } catch (InterruptedException e){
+                    System.out.println("Sleep thread has benn interrupted");
+                    e.printStackTrace(System.err);
+                }
+                screenlocker.notify();
+            }
+        }
+    },"Ping");
+    private Thread pong = new Thread(()->{
+        synchronized (screenlocker){
+            while (true){
+                screenlocker.notify();
+                try{
+                    System.out.println("Pong");
+                    Thread.sleep(1000);
+                    screenlocker.wait();
+                } catch (InterruptedException e){
+                    System.out.println("Sleep thread has benn interrupted");
+                    e.printStackTrace(System.err);
+                }
+            }
+        }
+
+    },"Pong");
+
+    public void play(){
+        ping.start();
+        pong.start();
+
+        try{
+            ping.join();
+            pong.join();
+        } catch (InterruptedException e){
+            e.printStackTrace(System.err);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new EndlessPingPong().play();
+    }
+}
