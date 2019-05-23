ab3c8dbc6da306b824381d6b0c6ff18e55528943
Igor Pystovit <igor.pystovit@gmail.com>
2019-05-23 03:42:10 +0300

    modified ArrayHandlerMenu.class / deleted Invoker.class

--- Arrays_task/Arrays_task/src/main/java/com/epam/igorpystovit/view/ArrayHandlerMenu.java
+++ Arrays_task/Arrays_task/src/main/java/com/epam/igorpystovit/view/ArrayHandlerMenu.java
@@ -1,20 +1,23 @@
 package com.epam.igorpystovit.view;
 
-import com.epam.igorpystovit.controller.Invoker;
+import com.epam.igorpystovit.controller.Controller;
+import com.epam.igorpystovit.model.Reader;
 
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class ArrayHandlerMenu implements Menu{
     private Map<String,String> menuItems;
     private Map<String,Runnable> menuActions;
-    private Invoker invoker = new Invoker();
+    private Controller controller = new Controller();
 
     @Override
     public Map<String, Runnable> initializeActions() {
+        ArrayHandlerMenu arrayHandlerMenu = new ArrayHandlerMenu();
         menuActions = new LinkedHashMap<>(){{
-            put("DELETE TWO TIMES OCCURRENCE",invoker::invokeDeleteTwoTimesOccurrence);
-            put("COMBINE ARRAYS",invoker::invokeCombineTwoArrays);
+            put("DELETE TWO TIMES OCCURRENCE",arrayHandlerMenu::invokeDeleteTwoTimesOccurrence);
+            put("COMBINE ARRAYS",arrayHandlerMenu::invokeCombineTwoArrays);
         }};
         return menuActions;
     }
@@ -26,6 +29,29 @@ public class ArrayHandlerMenu implements Menu{
             put("COMBINE ARRAYS","");
         }};
         return menuItems;
+    }
+
+    public Integer[] invokeDeleteTwoTimesOccurrence(){
+        System.out.println("Input array: ");
+        Integer[] userArray = Reader.readIntArray();
+        userArray = controller.deleteTwoTimesOccurrence(userArray);
+        printArray(userArray);
+        return userArray;
+    }
+
+    public Integer[] invokeCombineTwoArrays(){
+        System.out.println("Input first array: ");
+        Integer[] firstArray = Reader.readIntArray();
+        System.out.println("Input second array: ");
+        Integer[] secondArray = Reader.readIntArray();
+        Integer[] finalArray = controller.combineArrays(firstArray,secondArray);
+        printArray(finalArray);
+        return finalArray;
+    }
+
+
+    private void printArray(Integer[] array){
+        Arrays.stream(array).forEach(a -> System.out.print(a+" "));
     }
 
 
